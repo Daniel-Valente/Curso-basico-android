4f752c6ad87eb3986dd5154be8d44feb30fa7c4d
Carlos Daniel Valente Garcia <138070488+cdanielvalente@users.noreply.github.com>
2023-10-25 15:14:24 -0600

    Initial commit

---
+++ app/src/main/java/com/cdanielvalente/androidmaster/Variables.kt
@@ -0,0 +1,31 @@
+package com.cdanielvalente.androidmaster
+// val constante
+// var  
+/**
+ * Variables
+ */
+fun main() {
+    /**
+     * Variables Numericas
+     */
+    //Int -2,147,483,647 a 2,147,483,647
+    val age: Int = 30;
+
+    //Long -9,223,372,036,854,775,807 a 9,223,372,036,854,775,807
+    val example: Long = 30;
+    //Float
+    val pi: float = 3.1416;
+    //Double
+    val doubleExample: double = 3.1416;
+
+    /**
+     * Variables Alfanúmericas
+     */
+    //Char
+    val character: char = 'a';
+    //String
+    val name: string = "cdanielvalente";
+
+    //Boolean
+    val flag: boolean = true;
+}
