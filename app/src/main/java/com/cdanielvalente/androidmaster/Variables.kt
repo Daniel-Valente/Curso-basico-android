d1e88e2a00692502c7d958db8202c3982bfd7c34
Carlos Daniel Valente Garcia <138070488+cdanielvalente@users.noreply.github.com>
2023-10-25 15:21:16 -0600

    Utilidad de Variables

--- app/src/main/java/com/cdanielvalente/androidmaster/Variables.kt
+++ app/src/main/java/com/cdanielvalente/androidmaster/Variables.kt
@@ -1,6 +1,6 @@
 package com.cdanielvalente.androidmaster
 // val constante
-// var  
+// var
 /**
  * Variables
  */
@@ -10,22 +10,32 @@ fun main() {
      */
     //Int -2,147,483,647 a 2,147,483,647
     val age: Int = 30;
-
+    var age1: Int = 10;
     //Long -9,223,372,036,854,775,807 a 9,223,372,036,854,775,807
     val example: Long = 30;
     //Float
-    val pi: float = 3.1416;
+    val pi: Float = 3.1416f;
     //Double
-    val doubleExample: double = 3.1416;
+    val doubleExample: Double = 3.1416;
 
     /**
      * Variables Alfanúmericas
      */
     //Char
-    val character: char = 'a';
+    val character: Char = 'a';
     //String
-    val name: string = "cdanielvalente";
+    val name: String = "cdanielvalente";
 
     //Boolean
-    val flag: boolean = true;
+    val flag: Boolean = true;
+
+    /**
+     * Utilidad de variables
+     */
+    println( age + age1);
+    println( age - age1 );
+    println( age * age1 );
+    println( age / age1 );
+    println( age % age1 );
+
 }
